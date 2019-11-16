82b94d7e5f02a8a2b3f3bc544984ef9fa47e70a3
basketballrelativity <patmcfarla@gmail.com>
2019-11-16 09:58:24 -0500

    updating security

--- franchise_history.py
+++ franchise_history.py
@@ -21,6 +21,7 @@ HEADERS = {'Connection': 'close',
            'Host': 'stats.nba.com',
            'Origin': 'http://stats.nba.com',
            'Upgrade-Insecure-Requests': '1',
+           'Referer': 'stats.nba.com',
            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2)' + \
                          'AppleWebKit/537.36 (KHTML, like Gecko) ' + \
                          'Chrome/66.0.3359.117 Safari/537.36'}
